d666cba4e3879eb9c3e319bc2f49cad52e2946aa
Darchrow42 <31625345+Darchrow42@users.noreply.github.com>
2021-01-17 12:52:31 +0330

    Passed global check

--- Randomizer.py
+++ Randomizer.py
@@ -1,15 +1,12 @@
 import bpy
 import random
 
-
-class Randomizer(bpy.types.PropertyGroup):
-    min: bpy.props.FloatProperty(name="min",
-                                        description="Some elaborate description",
-                                        subtype="NONE")
-    max: bpy.props.FloatProperty(name="max",
-                                        description="Some elaborate description",
-                                        subtype="NONE")
-
+bpy.types.WindowManager.min= bpy.props.FloatProperty(name="min",
+                                    description="Some elaborate description",
+                                    subtype="NONE")
+bpy.types.WindowManager.max= bpy.props.FloatProperty(name="max",
+                                    description="Some elaborate description",
+                                    subtype="NONE")
 
 class RandomizeScale(bpy.types.Operator):
     bl_idname = "object.rndscale"
@@ -17,10 +14,10 @@ class RandomizeScale(bpy.types.Operator):
 
 
     def execute(self, context):
-        SRandom = random.uniform(0.8,1.2)
+        SRandom = random.uniform(context.scene.worth_group_tools.min,context.scene.worth_group_tools.max)
         for obj in bpy.context.selected_objects:
             obj.scale.x*=SRandom
-            SRandom = random.uniform(0.8,1.2)
+            SRandom = random.uniform(context.scene.worth_group_tools.min,context.scene.worth_group_tools.max)
         
         return {'FINISHED'}
 
@@ -32,45 +29,41 @@ class RandomizeScale(bpy.types.Operator):
 
 
 
-class RAND_Panel(bpy.types.Panel):
-    bl_label = "WorthGroup Tools Panel"
-    bl_category = "WorthGroup Tools Addon"
-    bl_space_type = "VIEW_3D"
-    bl_region_type = "UI"
 
-    def draw(self, context):
-        layout = self.layout
-        worth_group_tools = context.scene.worth_group_tools
-        split = layout.split()
-        col = split.column(align=True)
-        col.label(text="Column Two:")
-        col.prop(worth_group_tools, "min")
-        col.prop(worth_group_tools, "max")
-        
-        layout.label(text="Big Button:")
-        row = layout.row()
-        row.scale_y = 3.0
-        row.operator("object.rndscale")
+bl_label = "WorthGroup Tools Panel"
+bl_category = "WorthGroup Tools Addon"
+bl_space_type = "VIEW_3D"
+bl_region_type = "UI"
+
+def draw(self, context):
+    layout = self.layout
+    worth_group_tools = context.scene.worth_group_tools
+    split = layout.split()
+    col = split.column(align=True)
+    col.label(text="Range:")
+    col.prop(worth_group_tools, "min")
+    col.prop(worth_group_tools, "max")
+    
+    layout.label(text="Randomize:")
+    row = layout.row()
+    row.scale_y = 3.0
+    row.operator("object.rndscale")
 
 
-classes = (Randomizer,
-           RAND_Panel,
-           RandomizeScale)
+classes = (RandomizeScale)
 
 
 def register():
-    for cls in classes:
-        bpy.utils.register_class(cls)
+    bpy.utils.register_class(RandomizeScale)
         
-    bpy.types.Scene.worth_group_tools = bpy.props.PointerProperty(type=Randomizer)
+
 
 
 
 def unregister():
-    for cls in classes:
-        bpy.utils.unregister_class(cls)
+    bpy.utils.unregister_class(RandomizeScale)
  
-    del bpy.types.Scene.worth_group_tools
+
 
 
 
