47a736c3871f20e2a9b99d46b45fee570faf8c7d
Darchrow42 <31625345+Darchrow42@users.noreply.github.com>
2021-01-17 13:01:09 +0330

    Revert "Passed global check"
    This reverts commit d666cba4e3879eb9c3e319bc2f49cad52e2946aa.

--- Randomizer.py
+++ Randomizer.py
@@ -1,12 +1,15 @@
 import bpy
 import random
 
-bpy.types.WindowManager.min= bpy.props.FloatProperty(name="min",
-                                    description="Some elaborate description",
-                                    subtype="NONE")
-bpy.types.WindowManager.max= bpy.props.FloatProperty(name="max",
-                                    description="Some elaborate description",
-                                    subtype="NONE")
+
+class Randomizer(bpy.types.PropertyGroup):
+    min: bpy.props.FloatProperty(name="min",
+                                        description="Some elaborate description",
+                                        subtype="NONE")
+    max: bpy.props.FloatProperty(name="max",
+                                        description="Some elaborate description",
+                                        subtype="NONE")
+
 
 class RandomizeScale(bpy.types.Operator):
     bl_idname = "object.rndscale"
@@ -14,10 +17,10 @@ class RandomizeScale(bpy.types.Operator):
 
 
     def execute(self, context):
-        SRandom = random.uniform(context.scene.worth_group_tools.min,context.scene.worth_group_tools.max)
+        SRandom = random.uniform(0.8,1.2)
         for obj in bpy.context.selected_objects:
             obj.scale.x*=SRandom
-            SRandom = random.uniform(context.scene.worth_group_tools.min,context.scene.worth_group_tools.max)
+            SRandom = random.uniform(0.8,1.2)
         
         return {'FINISHED'}
 
@@ -29,41 +32,45 @@ class RandomizeScale(bpy.types.Operator):
 
 
 
+class RAND_Panel(bpy.types.Panel):
+    bl_label = "WorthGroup Tools Panel"
+    bl_category = "WorthGroup Tools Addon"
+    bl_space_type = "VIEW_3D"
+    bl_region_type = "UI"
 
-bl_label = "WorthGroup Tools Panel"
-bl_category = "WorthGroup Tools Addon"
-bl_space_type = "VIEW_3D"
-bl_region_type = "UI"
-
-def draw(self, context):
-    layout = self.layout
-    worth_group_tools = context.scene.worth_group_tools
-    split = layout.split()
-    col = split.column(align=True)
-    col.label(text="Range:")
-    col.prop(worth_group_tools, "min")
-    col.prop(worth_group_tools, "max")
-    
-    layout.label(text="Randomize:")
-    row = layout.row()
-    row.scale_y = 3.0
-    row.operator("object.rndscale")
+    def draw(self, context):
+        layout = self.layout
+        worth_group_tools = context.scene.worth_group_tools
+        split = layout.split()
+        col = split.column(align=True)
+        col.label(text="Column Two:")
+        col.prop(worth_group_tools, "min")
+        col.prop(worth_group_tools, "max")
+        
+        layout.label(text="Big Button:")
+        row = layout.row()
+        row.scale_y = 3.0
+        row.operator("object.rndscale")
 
 
-classes = (RandomizeScale)
+classes = (Randomizer,
+           RAND_Panel,
+           RandomizeScale)
 
 
 def register():
-    bpy.utils.register_class(RandomizeScale)
+    for cls in classes:
+        bpy.utils.register_class(cls)
         
-
+    bpy.types.Scene.worth_group_tools = bpy.props.PointerProperty(type=Randomizer)
 
 
 
 def unregister():
-    bpy.utils.unregister_class(RandomizeScale)
+    for cls in classes:
+        bpy.utils.unregister_class(cls)
  
-
+    del bpy.types.Scene.worth_group_tools
 
 
 
