dc5864372620a777105a579e8c6d850480c820ac
Darchrow42 <31625345+Darchrow42@users.noreply.github.com>
2021-01-17 12:41:18 +0330

    Before moving to global

--- Randomizer.py
+++ Randomizer.py
@@ -1,8 +1,9 @@
 import bpy
+import random
 
 
-class WorthGroupToolsSettings(bpy.types.PropertyGroup):
-    ass: bpy.props.FloatProperty(name="ass",
+class Randomizer(bpy.types.PropertyGroup):
+    min: bpy.props.FloatProperty(name="min",
                                         description="Some elaborate description",
                                         subtype="NONE")
     max: bpy.props.FloatProperty(name="max",
@@ -10,7 +11,28 @@ class WorthGroupToolsSettings(bpy.types.PropertyGroup):
                                         subtype="NONE")
 
 
-class WORTHGROUPTOOLS_PT_panel(bpy.types.Panel):
+class RandomizeScale(bpy.types.Operator):
+    bl_idname = "object.rndscale"
+    bl_label = "Randomize Scale In Range"
+
+
+    def execute(self, context):
+        SRandom = random.uniform(0.8,1.2)
+        for obj in bpy.context.selected_objects:
+            obj.scale.x*=SRandom
+            SRandom = random.uniform(0.8,1.2)
+        
+        return {'FINISHED'}
+
+
+
+
+
+
+
+
+
+class RAND_Panel(bpy.types.Panel):
     bl_label = "WorthGroup Tools Panel"
     bl_category = "WorthGroup Tools Addon"
     bl_space_type = "VIEW_3D"
@@ -18,28 +40,38 @@ class WORTHGROUPTOOLS_PT_panel(bpy.types.Panel):
 
     def draw(self, context):
         layout = self.layout
-        row = layout.row()
-
         worth_group_tools = context.scene.worth_group_tools
-        row.prop(worth_group_tools, "ass")
-        row.prop(worth_group_tools, "max")
+        split = layout.split()
+        col = split.column(align=True)
+        col.label(text="Column Two:")
+        col.prop(worth_group_tools, "min")
+        col.prop(worth_group_tools, "max")
         
+        layout.label(text="Big Button:")
+        row = layout.row()
+        row.scale_y = 3.0
+        row.operator("object.rndscale")
 
 
-classes = (WorthGroupToolsSettings,
-           WORTHGROUPTOOLS_PT_panel)
+classes = (Randomizer,
+           RAND_Panel,
+           RandomizeScale)
 
 
 def register():
     for cls in classes:
         bpy.utils.register_class(cls)
-    bpy.types.Scene.worth_group_tools = bpy.props.PointerProperty(type=WorthGroupToolsSettings)
+        
+    bpy.types.Scene.worth_group_tools = bpy.props.PointerProperty(type=Randomizer)
+
 
 
 def unregister():
     for cls in classes:
         bpy.utils.unregister_class(cls)
+ 
     del bpy.types.Scene.worth_group_tools
+
 
 
 if __name__ == "__main__":
